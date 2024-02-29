14a515eb84cdd2fb65fde236dbc8955bee65acdf
EshuDevs <yamindevs@gmail.com>
2024-02-29 11:14:08 +0100

    added transition animations to cart an Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Sidebar({openSidebar, setOpenSidebar}) {
   return (
-    <div style={{display: openSidebar ? "block" : "none"}} className='w-full h-full absolute top-0 left-0 p-4 z-10 bg-white'>
+    <div
+        className={`transition-transform ease-linear delay-0 duration-300 transform ${
+          openSidebar ? '-translate-x-0' : '-translate-x-full'
+        } w-full h-full absolute top-0 left-0 p-4 z-10 bg-white `}
+      > 
         <svg onClick={() => setOpenSidebar(!openSidebar)} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15" height="15" viewBox="0 0 50 50" className='float-right cursor-pointer'>
             <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path>
         </svg>
